8c03353399fa8658a462de6129e58aaf1b26a5b3
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-13 10:31:05 +0600

    Add files via upload

---
+++ asec12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class asec12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("First number: ");
+        double num1= input.nextDouble();
+
+        System.out.print(" Second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("Third number:");
+        double num3 = input.nextDouble();123
+
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("The average of the three numbers is: " + average);
+       
+    }
+    
+}
