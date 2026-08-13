b95610531bd74c6635c5ddd727f7540904e1fa7f
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-13 10:31:29 +0600

    Add files via upload

---
+++ asecprob13.java
@@ -0,0 +1,12 @@
+public class asecprob13 {
+    public static void main(String[] args) {
+         double width = 5.5;
+        double height = 8.5;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("Area is = " + area);
+        System.out.println("Perimeter is = " + perimeter);
+    }
+}
